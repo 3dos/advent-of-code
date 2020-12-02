72cd13442cd425984a2345eee45dbb68c4bb7f0e
Filipe Dos Santos <filipe.dossantos@gaming1.com>
2020-12-02 07:26:25 +0100

    add day 1

---
+++ day1/index.ts
@@ -0,0 +1,51 @@
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+const input = readFileSync(join(__dirname, './input.txt'), { encoding: 'utf8' })
+
+const formatedInput =
+    input
+        .split('\n')
+        .map(x => parseInt(x, 10))
+        .sort((a, b) => a - b)
+
+// Part 1
+const find2EntriesThatSumTo = (sum: number, list: number[]) => {
+    let headPos = 0
+    let tailPos = list.length - 1
+
+    while (list[headPos] + list[tailPos] != sum && headPos < tailPos) {
+        while(list[headPos] + list[tailPos] > sum)
+            tailPos -= 1
+
+        if (list[headPos] + list[tailPos] < sum) {
+            tailPos = list.length - 1
+            headPos += 1
+        }
+    }
+
+    return headPos >= tailPos
+        // No binom found
+        ? []
+        // Binom found
+        : [list[headPos], list[tailPos]]
+}
+
+// Part 2
+const find3EntriesThatSumTo = (sum: number, list: number[]) => {
+    for (const number of list) {
+        const foundBinom = find2EntriesThatSumTo(sum - number, list)
+        if (foundBinom.length == 2)
+            return [number, ...foundBinom]
+    }
+    return []
+}
+
+const binomProduct =
+    find2EntriesThatSumTo(2020, formatedInput).reduce((a, b) => a * b)
+
+const trinomProduct =
+    find3EntriesThatSumTo(2020, formatedInput).reduce((a, b) => a * b)
+
+console.log('binom product', binomProduct)
+console.log('trinom product', trinomProduct)
